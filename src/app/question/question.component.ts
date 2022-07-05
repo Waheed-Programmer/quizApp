90a764602f645af15d0ac510389c3cbea506f0eb
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-07-05 15:53:59 +0500

    Complete Quiz App in Angular

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,5 +1,6 @@
 import { NonNullAssert } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 import { QuestionService } from '../service/question.service';
 
 @Component({
@@ -16,11 +17,15 @@ export class QuestionComponent implements OnInit {
   selectedQuestion:any=null;
   correctAnswer:number =0;
   IncorrectAnswer:number =0;
+  Interval$:any;
+  progress:string="0";
+  IsquizCompleted:boolean=false;
   constructor(private questionservice: QuestionService ) { }
 
   ngOnInit(): void {
     this.name = localStorage.getItem("name")!;
     this.getAllQustion();
+    this.startCounter();
   }
   getAllQustion(){
     this.questionservice.getQuestionJson()
@@ -43,22 +48,72 @@ export class QuestionComponent implements OnInit {
   }
 
   answer(currentQno:number,option:any){
+    if(currentQno===this.questionList.length){
+      this.IsquizCompleted = true;
+      this.stopCounter();
+    }
     if(option.correct){
-      debugger
       this.points+=10;
       this.correctAnswer++;
       this.selectedQuestion=this.questionList[this.correctAnswer];
       this.currentQuestion++;
       this.selectedQuestion=this.questionList[this.currentQuestion];
+      this.getProgresss();
     }
     else{
-      debugger
+
       this.points-=10;
       this.currentQuestion++;
       this.selectedQuestion=this.questionList[this.correctAnswer];
       this.IncorrectAnswer++;
       this.selectedQuestion=this.questionList[this.IncorrectAnswer];
+      this.getProgresss();
     }
   }
+
+  startCounter(){
+
+    this.Interval$= interval(1000)
+    .subscribe(val=>{
+      this.counter--;
+      if(this.counter===0){
+        this.currentQuestion++;
+        this.selectedQuestion=this.questionList[this.currentQuestion];
+        this.counter=60;
+        this.points-=10
+      }
+    });
+    setTimeout(() => {
+      this.Interval$.unsubcribe();
+    }, 600000);
+  }
+
+  stopCounter(){
+    this.Interval$.unsubcribe();
+    this.counter=0;
+
+  }
+
+  resetCounter(){
+    this.stopCounter;
+    this.currentQuestion=0;
+    // this.selectedQuestion=this.questionList[this.currentQuestion];
+    this.points=0
+    this.counter=60;
+    this.startCounter();
+    this.progress = "0";
+  }
+
+  resetQuiz(){
+    this.resetCounter();
+    this.getAllQustion();
+    this.counter=60;
+  }
+
+  getProgresss(){
+    this.progress = ((this.currentQuestion/this.questionList.length)*100).toString();
+    return this.progress;
+  }
+
 
 }
